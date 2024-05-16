25793b89b949ffcb9aad5d6627fb80fb9be56f00
Erlend <eskeland.erlend@gmail.com>
2024-05-16 09:21:10 +0200

    faqside

---
+++ public/faq.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <header style="padding: 1px;">
+        <h1>FAQ</h1>
+        <a href="register.php">
+            <h1>Register</h1>
+        </a>
+    </header>
+    <style>
+        table {
+            width: 80%;
+            border-collapse: collapse;
+            background-color: white;
+
+        }
+
+        th,
+        td {
+            border: 1px solid #dddddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+    <div class="bakgrunn">
+
+        <h2>FAQ (Ofte stilte spørsmål)</h2>
+
+        <table>
+            <tr>
+                <th>Spørsmål</th>
+                <th>Svar</th>
+            </tr>
+            <tr>
+                <td>Hvordan oppretter jeg en ny konto?</td>
+                <td>Du kan opprette en ny konto ved å klikke på "Registrer" øverst på siden og følge instruksjonene.</td>
+            </tr>
+            <tr>
+                <td>Hvordan sender jeg inn en klage?</td>
+                <td>Du kan sende inn en klage ved å logge inn på kontoen din og gå til "Ticket" -siden. Fyll ut skjemaet og klikk på "Send inn".</td>
+            </tr>
+            <tr>
+                <td>Hvordan kan jeg endre passordet mitt?</td>
+                <td>Du kan endre passordet ditt ved å logge inn på kontoen din, gå til "Innstillinger" -siden og deretter til "Endre passord" -seksjonen.</td>
+            </tr>
+            <!-- Legg til flere spørsmål og svar etter behov -->
+        </table>
+    </div>
+    <footer></footer>
+</body>
+
+</html>
